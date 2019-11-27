c65e9a8ec46c61631a7a188d4e6f540ecd934527
ahmed-bahaa <a.bahaarefaii@gmail.com>
2019-11-28 01:59:01 +0200

    lesson 2.1

--- variables.go
+++ variables.go
@@ -6,7 +6,22 @@ import (
 
 var x int = 7
 
+// package scope 
+// this way to organize 
+var (
+	name string = "ahmed"
+	age int = 50
+)
+
+var (
+	name2 string = "ahmed2"
+	age2 int = 500	
+)
+
+
+
 func main(){
+
 	// the first way
 	var x int 
 	x = 2
@@ -21,6 +36,7 @@ func main(){
 	fmt.Println(y)
 	fmt.Printf("%v,%T",z,z)
 	fmt.Println(55)
+	fmt.Println(name,name2,age,age2)
 }
 
 // notes: 
