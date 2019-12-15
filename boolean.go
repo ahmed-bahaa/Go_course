68f5d571fb6dd36ec32152c7f87116fc4b3a201b
ahmed-bahaa <a.bahaarefaii@gmail.com>
2019-12-15 17:26:26 +0200

    booleans

---
+++ boolean.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println(" 5 is greater than 2 : ", 5 > 2)
+	fmt.Println(" 5 is less than 2 : ", 5 < 2)
+	fmt.Println(" 5 is greater than or equal 2 : ", 5 >= 2)
+	fmt.Println(" 5 is greater than 5 or equal : ", 5 >= 5)
+
+	fmt.Println(" 3 is equal 3.0 : ", 3 == 3.0)
+	fmt.Println(" 3 is not equal 3.0 : ", 3 != 3.0)
+
+	/*
+	    we can also use this by
+
+	    if err != nil {
+
+	    // handle the error
+	  }
+
+	*/
+}
